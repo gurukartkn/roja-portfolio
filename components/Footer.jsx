b52209c1c922af1bb09b3fb27e1b8619f5a0f772
Roja1801 <83115464+Roja1801@users.noreply.github.com>
2024-02-24 11:57:14 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="text-[#429BEC] dark:text-white p-5 font-roboto">
-      © 2022 All Rights Reserved by{" "}
+      © 2024 All Rights Reserved by{" "}
       <Link href="/">
         <a className="font-poppins text-[#063A8D] dark:text-[#FA58B6] font-bold">
           Roja.
